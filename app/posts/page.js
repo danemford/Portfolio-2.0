e3fa1da1031904cb4b84cd74385d92852fd3e8a9
danemford <danemford@gmail.com>
2023-07-10 09:10:24 +1000

    added mdx functionality for posts

--- app/posts/page.js
+++ app/posts/page.js
@@ -1,7 +1,9 @@
+import Hello from '../../articles/hello.mdx'
+
 export default function Posts() {
     return (
         <main className="mx-auto mt-52 mb-24 max-w-3xl px-6 w-full grow">
-          <div>Posts</div>
+          <div><Hello /></div>
         </main>
     )
   }
