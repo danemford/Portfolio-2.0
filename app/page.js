fa5a53e545ad2e094f097032919f298128db6ffd
Dane Ford <dford@anglican.asn.au>
2023-09-21 22:36:56 +1000

    sort posts on homepage by date, and limit to 4

--- app/page.js
+++ app/page.js
@@ -10,6 +10,15 @@ const Page = async () => {
 
   const posts = await getAllPostsMeta()
 
+  function parseDateString(dateString) {
+    // Remove the 'th', 'st', 'nd', 'rd' from the date string
+    const cleanedDate = dateString.replace(/(st|nd|rd|th),/, ',');
+  
+    // Parse the cleaned date string into a Date object
+    return new Date(cleanedDate);
+  }
+  
+
   return (
     <main className="mx-auto mt-52 mb-24 max-w-3xl px-6 w-full grow">
       <section className="mb-24 flex flex-col-reverse gap-y-4 gap-x-10 md:flex-row items-start md:items-center">
@@ -29,21 +38,25 @@ const Page = async () => {
         <h2 className="mb-8 text-2xl font-bold">
           Recent Posts
         </h2>
-        <div className="">
-        {posts?.map(post => (
-          <div key={post?.title} className="mb-6">
-            <Link
-              href={`posts/${post.slug}`}
-              className=""
-            >
-              <h3 className='font-semibold'>{post.title}</h3>
-              <p className='text-sm font-light'>{post.description}</p>
-              <time className='text-[12px] text-gray-400 mb-6'>
-                {post.publishDate}
-              </time>
-            </Link>
-          </div>
-        ))}
+        <div>
+          {posts?.sort((a, b) => parseDateString(b.publishDate) - parseDateString(a.publishDate))
+            .slice(0, 4)
+            .map(post => (
+              <div className="mb-6" key={post?.title}>
+                <Link href={`posts/${post.slug}`}>
+                    <h3 className='font-semibold'>{post.title}</h3>
+                    <p className='text-sm font-light'>{post.description}</p>
+                    <time className='text-[12px] text-gray-400'>
+                      {post.publishDate}
+                    </time>
+                </Link>
+              </div>
+          ))}
+        </div>
+        <div>
+          <Link href={`posts/`}>
+          <h3 className="mb-8 font-semibold">See All Posts</h3>
+          </Link>
         </div>
       </section>
       <section className="mb-24">
