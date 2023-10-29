9aee069e252da49058b2fb8d6fc6294b034e1df2
Dane Ford <danemford@gmail.com>
2023-10-29 19:58:46 +1100

    updated footer and contact form

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -6,8 +6,8 @@ const Footer = () => {
 
     return (
         <footer className="border-t-sky-500/75 border-t px-12">
-            <div className='flex justify-between my-6'>
-                <p className='flex'>You can also find me here<LiaHandPointRightSolid className='h-6 w-6 ml-1'/> </p>
+            <div className='flex justify-center my-6'>
+                <p className='flex items-center'>You can also find me here<LiaHandPointRightSolid className='h-6 w-6 ml-1'/> </p>
                 <div className='flex px-6'>
                     <Link href='https://github.com/danemford'>
                         <AiFillGithub className='h-8 w-8 mr-2'/>
